ca4bda889ed7960645c5b5c01afa1f53fe2254c2
ofaruk84 <korkmazfaruk84@gmail.com>
2022-04-13 03:37:40 +0300

    Push Fix

---
+++ Core/CrossCuttingConcerns/Validation/FluentValidation/ValidationTool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Core.CrossCuttingConcerns.Validation.FluentValidation
+{
+    public static class ValidationTool<T>
+    {
+        public static bool Validate(IValidator validator,T entity)
+        {
+            var validationContext = new ValidationContext<T>(entity);
+            var result = validator.Validate(validationContext);
+
+            return result.IsValid;
+        }
+    }
+}
--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -3,22 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Core.DataAccess
 {
     public interface IEntityRepository<T> where T : class, IEntity, new()
     {
+        //push Fix
 
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter = null);
 
-        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
 
-        T Get(Expression<Func<T, bool>> filter);
+        Task AddAsync(T entity);
 
-        void Add(T entity);
+        Task UpdateAsync(T entity);
 
-        void Update(T entity);
-
-        void Delete(T entity);
+        Task DeleteAsync(T entity);
 
 
 
